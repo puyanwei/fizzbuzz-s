eb7fa9f44239f53ea8e21666289cd1a394bb8d44
Puyan Wei <puyanwei@gmail.com>
2018-02-04 10:53:32 +0000

    passing test for controller

--- spec/test.js
+++ spec/test.js
@@ -1,14 +1,23 @@
-describe("#fizz", function() {
-  it("integar is divisible by 3", function() {
-    expect(fizz(9)).toEqual("Fizz");
+describe("#divisibleByIntegar", function() {
+  it("integar is divisible by 3 and should return Fizz", function() {
+    expect(divisibleByIntegar(9, 3, "Fizz")).toEqual("Fizz");
   });
-  it("integar is not divisible by 3", function() {
-    expect(fizz(8)).toEqual(8);
+  it("integar is not divisible by 3 and should return its original number", function() {
+    expect(divisibleByIntegar(8, 3, "Fizz")).toEqual(8);
   });
-});
+  it("integar is divisible by 5", function() {
+    expect(divisibleByIntegar(10, 5, "Buzz")).toEqual("Buzz");
+  });
+  it("integar is not divisible by 5", function() {
+    expect(divisibleByIntegar(11, 5, "Buzz")).toEqual(11);
+  });
 
-describe("#buzz", function() {
-  xit("integar is divisible by 5", function() {
-    expect(buzz(10).toEqual("Buzz"));
+  describe("#controller", function() {
+    it("integar is divisible by 3 and 5 and returns FizzBuzz", function() {
+      expect(controller(15)).toEqual("FizzBuzz");
+    });
+    it("integar not is divisible by 3 and 5 and returns FizzBuzz", function() {
+      expect(controller(11)).toEqual(11);
+    });
   });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,18 @@
-function fizz(number) {
-  if (number % 3 == 0) {
-    return "Fizz";
+function divisibleByIntegar(number, divisibleBy, result) {
+  if (number % divisibleBy == 0) {
+    return result;
+  } else {
+    return number;
+  }
+}
+
+function controller(number) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
+  } else if (number % 3 == 0) {
+    divisibleByIntegar(number, 3, "Fizz");
+  } else if (number % 5 == 0) {
+    divisibleByIntegar(number, 5, "Buzz");
   } else {
     return number;
   }
